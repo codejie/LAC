daba5d3236c1d3bf88a66f7f692d4e7bf6197fbc
codejie <codejie@gmail.com>
2013-05-01 23:51:02 +0800

    Continue to use viewSwitcher to shift word and data like LAC.

--- src/jie/android/lac/fragment/DictionaryFragment.java
+++ src/jie/android/lac/fragment/DictionaryFragment.java
@@ -2,18 +2,14 @@ package jie.android.lac.fragment;
 
 import com.handmark.pulltorefresh.library.PullToRefreshBase;
 import com.handmark.pulltorefresh.library.PullToRefreshBase.Mode;
-import com.handmark.pulltorefresh.library.PullToRefreshBase.OnPullEventListener;
 import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
-import com.handmark.pulltorefresh.library.PullToRefreshBase.State;
 import com.handmark.pulltorefresh.library.PullToRefreshListView;
 
 import jie.android.lac.R;
 import jie.android.lac.fragment.data.DictionaryFragmentListAdapter;
 import jie.android.lac.fragment.data.DictionaryFragmentListAdapter.OnRefreshResultListener;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -22,17 +18,20 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ViewSwitcher;
 
 public class DictionaryFragment extends ContentFragment implements OnRefreshResultListener, OnRefreshListener<ListView>, OnItemClickListener  {
 	
 	private static final String Tag = DictionaryFragment.class.getName();
 
-	private PullToRefreshListView pullList = null;
+	private ViewSwitcher viewSwitcher = null;
 	
+	private PullToRefreshListView pullList = null;	
 	private DictionaryFragmentListAdapter adapter = null;
 	
 	public DictionaryFragment() {
-		super(R.layout.fragment_dictionary_list);
+		//super(R.layout.fragment_dictionary_list);
+		super(R.layout.fragment_dictionary_switcher);
 	}
 
 	@Override
@@ -46,15 +45,18 @@ public class DictionaryFragment extends ContentFragment implements OnRefreshResu
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 		
+		viewSwitcher = (ViewSwitcher) view.findViewById(R.id.viewSwitcher);
+		
 		adapter = new DictionaryFragmentListAdapter(this.getLACActivity(), this.getLACActivity().getServiceAccess(), this);
 		
-		pullList = (PullToRefreshListView) view.findViewById(R.id.pull_refresh_list);
+		pullList = (PullToRefreshListView) viewSwitcher.findViewById(R.id.pull_refresh_list);
 		pullList.setMode(Mode.PULL_FROM_END);
 		pullList.setOnRefreshListener(this);
+		pullList.setOnItemClickListener(this);
 	
 		pullList.setAdapter(adapter);		
 		
-		Button btn = (Button) view.findViewById(R.id.button1);
+		Button btn = (Button) viewSwitcher.findViewById(R.id.button1);
 		btn.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -71,8 +73,7 @@ public class DictionaryFragment extends ContentFragment implements OnRefreshResu
 
 	@Override
 	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-		// TODO Auto-generated method stub
-		
+		viewSwitcher.showNext();
 	}
 
 	@Override
